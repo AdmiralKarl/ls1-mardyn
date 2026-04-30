360192109e590a9f690a08f8966f803f6acfc989
AdmiralKarl <46034594+AdmiralKarl@users.noreply.github.com>
2026-04-30 15:21:39 +0200

    ALLLoadBalancer added a warning for settings that have not been fully tested

--- src/parallel/ALLLoadBalancer.cpp
+++ src/parallel/ALLLoadBalancer.cpp
@@ -23,19 +23,23 @@ ALLLoadBalancer::ALLLoadBalancer(std::array<double, DIMgeom> localBoxMin, std::a
 
 void ALLLoadBalancer::readXML(XMLfileUnits& xmlconfig){
 	ALL::LB_t mode = ALL::LB_t::UNIMPLEMENTED;
+	bool not_tested = false;
 
 	std::string loadBalancer("TENSOR");
 	xmlconfig.getNodeValue("mode", loadBalancer);
 	
 	if (loadBalancer == "STAGGERED") {
 		mode = ALL::LB_t::STAGGERED;
+		not_tested = true;
 	} else if (loadBalancer == "TENSOR") {
 		mode = ALL::LB_t::TENSOR;
 	} else if (loadBalancer == "FORCEBASED") {
-		mode = ALL::LB_t::FORCEBASED; 
+		mode = ALL::LB_t::FORCEBASED;
+		not_tested = true; 
 	} else if (loadBalancer == "ALL_VORONOI_ACTIVE") {
 		#ifdef ALL_VORONOI_ACTIVE
 			mode = ALL::LB_t::VORONOI;
+			not_tested = true;
 		#else
 			std::ostringstream error_message;
 			error_message << "ALLLoadBalancer: ALL libery has VORONOI not active. Aborting! Please select a valid option!";
@@ -43,8 +47,10 @@ void ALLLoadBalancer::readXML(XMLfileUnits& xmlconfig){
 		#endif
 	} else if (loadBalancer == "HISTOGRAM") {
 		mode = ALL::LB_t::HISTOGRAM;
+		not_tested = true;
 	} else if (loadBalancer == "TENSOR_MAX") {
 		mode = ALL::LB_t::TENSOR_MAX;
+		not_tested = true;
 	} else {
 		std::ostringstream error_message;
 		error_message << "ALLLoadBalancer: Unsupported load balancer " << loadBalancer << " was selected. Aborting! Please select a valid option!";
@@ -52,6 +58,10 @@ void ALLLoadBalancer::readXML(XMLfileUnits& xmlconfig){
 	}
 
 	Log::global_log->info() << "ALLLoadBalancer: using the " << loadBalancer << " load balancer" << std::endl;
+	if (not_tested) {
+		Log::global_log->warning() << "ALLLoadBalancer: the " << loadBalancer << " load balancer has not been fully tested in LS1-Mardyn and may produce unexpected results" << std::endl;
+	}
+
 
 	_all = std::make_unique<ALL::ALL<double, double>>(ALL::TENSOR, DIMgeom, _gamma);
 	_all->setCommunicator(_comm);
