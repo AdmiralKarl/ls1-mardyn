29643f01d236bde54ed94cc383f9086a188a4c12
AdmiralKarl <46034594+AdmiralKarl@users.noreply.github.com>
2026-05-17 23:14:47 +0200

    minor optimization

--- src/parallel/GeneralDomainDecomposition.cpp
+++ src/parallel/GeneralDomainDecomposition.cpp
@@ -365,35 +365,35 @@ void GeneralDomainDecomposition::migrateParticles(Domain* domain, ParticleContai
 								true , false);
 			}
 		}
-	#elif false
-		{
-			std::vector<Molecule> dummy;
-			for (auto& sender : sendNeighbors) {
-				sender.initSend(particleContainer, _comm, _mpiParticleType, LEAVING_ONLY, dummy,
-								false /*don't use invalid particles*/, false /*do halo position change*/,
-								true /*removeFromContainer*/);
-			}
-			emigrants = particleContainer->rebuildFilter(newMin.data(), newMax.data());
-			// particleContainer->addParticles(emigrants); 
-		}
 	#else
-	{
-		std::vector<Molecule> dummy;
-		for (auto& sender : sendNeighbors) {
-			sender.initSend(particleContainer, _comm, _mpiParticleType, LEAVING_ONLY, dummy,
-							false /*don't use invalid particles*/, true /*do halo position change*/,
-							true /*removeFromContainer*/);
-		}
+		{
+			if (particleContainer->isInvalidParticleReturner()) {
+				std::vector<Molecule> dummy;
+				for (auto& sender : sendNeighbors) {
+					sender.initSend(particleContainer, _comm, _mpiParticleType, LEAVING_ONLY, dummy,
+									false /*don't use invalid particles*/, false /*do halo position change*/,
+									true /*removeFromContainer*/);
+				}
+				emigrants = particleContainer->rebuildFilter(newMin.data(), newMax.data());
+				// particleContainer->addParticles(emigrants); 
+			} else {
+				std::vector<Molecule> dummy;
+				for (auto& sender : sendNeighbors) {
+					sender.initSend(particleContainer, _comm, _mpiParticleType, LEAVING_ONLY, dummy,
+									false /*don't use invalid particles*/, true /*do halo position change*/,
+									true /*removeFromContainer*/);
+				}
 
-		std::vector<Molecule> ownMolecules{};
-		ownMolecules.reserve(particleContainer->getNumberOfParticles());
-		for (auto iter = particleContainer->iterator(ParticleIterator::ONLY_INNER_AND_BOUNDARY); iter.isValid(); ++iter) {
-			ownMolecules.push_back(*iter);
+				std::vector<Molecule> ownMolecules{};
+				ownMolecules.reserve(particleContainer->getNumberOfParticles());
+				for (auto iter = particleContainer->iterator(ParticleIterator::ONLY_INNER_AND_BOUNDARY); iter.isValid(); ++iter) {
+					ownMolecules.push_back(*iter);
+				}
+				particleContainer->clear();
+				particleContainer->rebuild(newMin.data(), newMax.data());
+				particleContainer->addParticles(ownMolecules);
+			}	
 		}
-		particleContainer->clear();
-		particleContainer->rebuild(newMin.data(), newMax.data());
-		particleContainer->addParticles(ownMolecules);
-	}
 	#endif
 
 	bool allDone = false;
